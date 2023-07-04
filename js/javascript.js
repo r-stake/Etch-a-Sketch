a4b604e2ab19709d2cf425f0eb46ea18dde53faf
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-04 17:07:33 +0300

    Add events for coloring in the squares

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,32 @@
 // Create a 16x16 grid of square divs
+const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
-    const container = document.querySelector(".container");
     const divSquare = document.createElement("div");
     container.appendChild(divSquare);
 }
+
+let mousePressed = false;
+
+const grid = document.querySelectorAll(".container>div");
+grid.forEach(item => {
+    item.addEventListener("mousedown", function () {
+        mousePressed = true;
+        item.style.backgroundColor = "black";
+    })
+});
+
+grid.forEach(item => {
+    item.addEventListener("mouseover", function() {
+        if (mousePressed) {
+            item.style.backgroundColor = "black";
+        }
+    })
+});
+
+grid.forEach(item => {
+    item.addEventListener("mouseup", function() {
+        mousePressed = false;
+    })
+})
+
+console.log(grid);
