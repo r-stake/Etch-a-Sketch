722fb6101d3915c605981e419670eed8aec46bdb
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-06 16:20:51 +0300

    Add color selection functionality
    Created a function "getColor" to extract clicked color square rgb values
    and update them through "setColor" function.
    
    Added a toggle effect for random color selection button.

--- js/javascript.js
+++ js/javascript.js
@@ -9,6 +9,7 @@ const container = document.querySelector(".container");
 const btnResizeGrid = document.querySelector(".grid-size");
 const btnClearGrid = document.querySelector(".clear-grid");
 const btnRandomizeColor = document.querySelector(".randomize-color");
+const colorSelections = document.querySelectorAll(".color-selection>div");
 
 function createGrid(n) {
     const grid = document.querySelectorAll(".container>div");
@@ -27,12 +28,41 @@ function createGrid(n) {
 function resizeGrid() {
     let userInput = 0;
     while (!userInput || userInput > 100) {
-        userInput = +prompt("Enter grid size");
+        userInput = +prompt("Enter the number of squares per side (1-100)");
     }
     n = userInput;
     userInput = 0
     createGrid(n);
 }
+
+function getColor(element) {
+    const computedStyle = window.getComputedStyle(element);
+    const color = computedStyle.backgroundColor;
+    const rgbValues = color.match(/\d+/g);
+    console.log(rgbValues);
+    const rgbRedValue = parseInt(rgbValues[0]);
+    const rgbGreenValue = parseInt(rgbValues[1]);
+    const rgbBlueValue = parseInt(rgbValues[2]);
+    console.log(rgbRedValue);
+    console.log(rgbGreenValue);
+    console.log(rgbBlueValue);
+    return {
+        red: rgbRedValue,
+        green: rgbGreenValue,
+        blue: rgbBlueValue
+    };
+}
+
+function setColor(rgbRedValue, rgbGreenValue, rgbBlueValue) {
+    rgbRed = rgbRedValue;
+    rgbGreen = rgbGreenValue;
+    rgbBlue = rgbBlueValue;
+}
+
+// function updateColorUI() {
+//     const currentlySelectedColor = document.querySelector(`.${selector}`);
+//     currentlySelectedColor = selector.classList.add("highlighted-color");
+// }
 
 function clearGrid() {
     const grid = document.querySelectorAll(".container>div")
@@ -79,17 +109,25 @@ function sketch() {
 }
 
 btnResizeGrid.addEventListener("click", resizeGrid);
+colorSelections.forEach(selection => {
+    selection.addEventListener("click", function() {
+        const colorValues = getColor(this);
+        setColor(colorValues.red, colorValues.green, colorValues.blue);
+    });
+});
 btnClearGrid.addEventListener("click", clearGrid);
 btnRandomizeColor.addEventListener("click", () => {
-    if (isRandomColor) {
+    if (isRandomColor === true) {
         isRandomColor = false;
         rgbRed = 0;
         rgbGreen = 0;
         rgbBlue = 0;
+        btnRandomizeColor.classList.remove("selected");
     } else {
         isRandomColor = true;
+        btnRandomizeColor.classList.add("selected");
     }
-})
+});
 
 container.addEventListener("mouseenter", () => {
     sketchTrue = true;
